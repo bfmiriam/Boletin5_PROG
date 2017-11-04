5cbc6aff173c024a5a0a8e650d2010df757e3a24
miriam <bfmiriam>
2017-11-04 14:20:41 +0100

    Inicio proyecto

---
+++ boletin5_7/src/boletin5_7/Clase1.java
@@ -0,0 +1,28 @@
+
+package boletin5_7;
+
+
+public class Clase1 {
+    
+    public int num;
+    public int lado;
+    public int base, altura;
+    final static double PI=3.14;
+    public double radio;
+    
+public Clase1(){
+    
+}
+    
+public Clase1(int num, int lado, int base, int altura, int radio){
+    this.num=num;
+    this.lado=lado;
+    this.base=base;
+    this.altura=altura;
+    this.radio=radio;
+   
+    }
+    
+    
+    
+}
